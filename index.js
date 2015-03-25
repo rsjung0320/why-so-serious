c199963bb6bbfd1d70775041ee2f920a7a74ad97
rsjung <rsjung0320@gmail.com>
2015-03-25 21:21:12 +0900

    test

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function(request, response) {
-//   response.send('MJ_____Hello World!   123123123');
-// });
+app.get('/', function(request, response) {
+
+    fs.readFile('index.html', function (error, data) {
+        response.send(data.toString());
+        
+    });
+
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
