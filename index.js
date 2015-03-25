fb9249182d54a48339693ebb9871cae1b5a82117
rsjung <rsjung0320@gmail.com>
2015-03-25 21:49:43 +0900

    test

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var Client = require('mariasql');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-
     fs.readFile('index.html', function (error, data) {
         response.send(data.toString());
-        
-    });
 
+        var c = new Client();
+
+		c.connect({
+		  host: 'http://kpig7.synology.me:3306',
+		  user: 'admin',
+		  password: 'admin!'
+		});
+
+		c.on('connect', function() {
+		   console.log('Client connected');
+		 })
+		 .on('error', function(err) {
+		   console.log('Client error: ' + err);
+		 })
+		 .on('close', function(hadError) {
+		   console.log('Client closed');
+		 });
+
+		c.query('SHOW DATABASES')
+		 .on('result', function(res) {
+		   res.on('row', function(row) {
+		     console.log('Result row: ' + inspect(row));
+		   })
+		   .on('error', function(err) {
+		     console.log('Result error: ' + inspect(err));
+		   })
+		   .on('end', function(info) {
+		     console.log('Result finished successfully');
+		   });
+		 })
+		 .on('end', function() {
+		   console.log('Done with all results');
+		 });
+
+		c.end();
+    });
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+
 
 
 // 모듈을 추출합니다.
