444cc4b5071a215a2747e320cfcb18c24b10ec30
rsjung <rsjung0320@gmail.com>
2015-03-25 21:52:47 +0900

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var Client = require('mariasql');
+// var Client = require('mariasql');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
